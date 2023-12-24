e4e15842f4ca73bbb8a4fc48a0c6abab0ad20129
Marginal <osipow.sergey2007@yandex.ru>
2023-12-24 20:29:28 +0300

    add ghosts and useful functions

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import pygame
 
 
+class Direction():
+    LEFT = 0
+    UP = 1
+    RIGHT = 2
+    DOWN = 3,
+    NONE = 4
+
+
+def translate_screen_to_maze(coords, size=32):
+    return int(coords[0] / size), int(coords[1] / size)
+
+
+def translate_maze_to_screen(coords, size=32):
+    return coords[0] * size, coords[1] * size
+
+
 class Object:
     def __init__(self, surface, x, y,
                  size: int, color=(255, 0, 0),
@@ -128,6 +144,12 @@ class Controller:
 
         self.size = (0, 0)
         self.convert()
+        self.ghost_colors = [
+            (255, 184, 255),
+            (255, 0, 20),
+            (0, 255, 255),
+            (255, 184, 82)
+        ]
 
     def convert(self):
         for x, row in enumerate(self.ascii_maze):
@@ -146,6 +168,57 @@ class Controller:
             self.numpy_maze.append(binary_row)
 
 
+class MovableObject(Object):
+    def __init__(self, surface, x, y, size: int, color=(255, 0, 0), circle: bool = False):
+        super().__init__(surface, x, y, size, color, circle)
+        self.cur_dir = Direction.NONE
+        self.dir_buffer = Direction.NONE
+        self.last_dir = Direction.NONE
+        self.queue = []
+        self.next_target = None
+
+    def get_next_location(self):
+        if len(self.queue) == 0:
+            return None
+        else:
+            self.queue.pop(0)
+
+    def set_direction(self, direction):
+        self.cur_dir = direction
+        self.dir_buffer = direction
+
+    def collides_with_wall(self, position):
+        collision_rect = pygame.Rect(position[0], position[1], self.size, self.size)
+        collides = False
+        walls = self.renderer.get_walls()
+        for wall in walls:
+            collides = collision_rect.colliderect(wall.get_shape())
+            if collides:
+                break
+        return collides
+
+    def check_collision_in_direction(self, direction: Direction):
+        des_position = (0, 0)
+        if direction == Direction.NONE:
+            return False, des_position
+        if direction == Direction.UP:
+            des_position = (self.x, self.y - 1)
+        elif direction == Direction.DOWN:
+            des_position = (self.x, self.y + 1)
+        elif direction == Direction.LEFT:
+            des_position = (self.x - 1, self.y)
+        elif direction == Direction.RIGHT:
+            des_position = (self.x + 1, self.y)
+
+        return self.collides_with_wall(des_position), des_position
+
+
+class Ghost(MovableObject):
+    def __init__(self, surface, x, y, size: int, game_controller, color=(255, 0, 0)):
+        super().__init__(surface, x, y, size, color, False)
+        self.game_controller = game_controller
+
+
 if __name__ == "__main__":
     unified_size = 32
     pacman_game = Controller()
@@ -156,5 +229,11 @@ if __name__ == "__main__":
         for x, column in enumerate(row):
             if column == 0:
                 game_renderer.add_wall(Wall(game_renderer, x, y, unified_size))
+
+    for i, ghost_spawn in enumerate(pacman_game.ghost_spawns):
+        translated = translate_maze_to_screen(ghost_spawn)
+        ghost = Ghost(game_renderer, translated[0], translated[1], unified_size, pacman_game,
+                      pacman_game.ghost_colors[i % 4])
+        game_renderer.add_game_object(ghost)
 
     game_renderer.tick(120)
