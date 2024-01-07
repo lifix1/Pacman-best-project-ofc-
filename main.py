369ef0cf6f8002c624bbaa2610fae8369a757757
Nostraw <Nostraw1@yandex.ru>
2024-01-07 18:55:20 +0300

    Reworked map renderer and some minor changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pygame
+import sys
+import os
 
-
-class Direction():
-    LEFT = 0
-    UP = 1
-    RIGHT = 2
-    DOWN = 3,
-    NONE = 4
+LEFT = 0
+UP = 1
+RIGHT = 2
+DOWN = 3,
+NONE = 4
 
 
 def translate_screen_to_maze(coords, size=32):
@@ -17,12 +17,20 @@ def translate_maze_to_screen(coords, size=32):
     return coords[0] * size, coords[1] * size
 
 
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
 class Object:
     def __init__(self, surface, x, y,
                  size: int, color=(255, 0, 0),
                  circle: bool = False):
         self.size = size
-        self.renderer = surface
+        self.render = surface
         self.surface = surface.screen
         self.y = y
         self.x = x
@@ -62,7 +70,7 @@ class Wall(Object):
         super().__init__(surface, x * size, y * size, size, color)
 
 
-class Renderer:
+class Render:
     def __init__(self, width: int, height: int):
         pygame.init()
         self.width = width
@@ -76,7 +84,6 @@ class Renderer:
         self.points = []
 
     def tick(self, fps: int):
-        black = (0, 0, 0)
         while not self.done:
             for game_object in self.game_objects:
                 game_object.tick()
@@ -84,8 +91,8 @@ class Renderer:
 
             pygame.display.flip()
             self.clock.tick(fps)
-            self.screen.fill(black)
-            self.handle_events()
+            self.screen.fill((0, 0, 0))
+            self.events_helper()
         print("Game over")
 
     def add_game_object(self, obj: Object):
@@ -107,20 +114,18 @@ class Renderer:
         self.add_game_object(hero)
         self.hero = hero
 
-    def handle_events(self):
+    def events_helper(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.done = True
-
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]:
-            self.hero.set_direction(Direction.UP)
-        elif pressed[pygame.K_LEFT]:
-            self.hero.set_direction(Direction.LEFT)
-        elif pressed[pygame.K_DOWN]:
-            self.hero.set_direction(Direction.DOWN)
-        elif pressed[pygame.K_RIGHT]:
-            self.hero.set_direction(Direction.RIGHT)
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                self.hero.set_direction(UP)
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+                self.hero.set_direction(LEFT)
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                self.hero.set_direction(DOWN)
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+                self.hero.set_direction(RIGHT)
 
     def get_game_objects(self):
         return self.game_objects
@@ -128,41 +133,9 @@ class Renderer:
 
 class Controller:
     def __init__(self):
-        self.ascii_maze = [
-            "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-            "XP           XX            X",
-            "X XXXX XXXXX XX XXXXX XXXX X",
-            "X XXXX XXXXX XX XXXXX XXXX X",
-            "X XXXX XXXXX XX XXXXX XXXX X",
-            "X                          X",
-            "X XXXX XX XXXXXXXX XX XXXX X",
-            "X XXXX XX XXXXXXXX XX XXXX X",
-            "X      XX    XX    XX      X",
-            "XXXXXX XXXXX XX XXXXX XXXXXX",
-            "XXXXXX XXXXX XX XXXXX XXXXXX",
-            "XXXXXX XX          XX XXXXXX",
-            "XXXXXX XX XXXXXXXX XX XXXXXX",
-            "XXXXXX XX X   G  X XX XXXXXX",
-            "          X G    X          ",
-            "XXXXXX XX X   G  X XX XXXXXX",
-            "XXXXXX XX XXXXXXXX XX XXXXXX",
-            "XXXXXX XX          XX XXXXXX",
-            "XXXXXX XX XXXXXXXX XX XXXXXX",
-            "XXXXXX XX XXXXXXXX XX XXXXXX",
-            "X            XX            X",
-            "X XXXX XXXXX XX XXXXX XXXX X",
-            "X XXXX XXXXX XX XXXXX XXXX X",
-            "X   XX       G        XX   X",
-            "XXX XX XX XXXXXXXX XX XX XXX",
-            "XXX XX XX XXXXXXXX XX XX XXX",
-            "X      XX    XX    XX      X",
-            "X XXXXXXXXXX XX XXXXXXXXXX X",
-            "X XXXXXXXXXX XX XXXXXXXXXX X",
-            "X                          X",
-            "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        ]
+        self.ascii_maze = load_level('map1.txt')
 
-        self.numpy_maze = []
+        self.maze = []
         self.point_spaces = []
         self.reachable_spaces = []
         self.ghost_spawns = []
@@ -190,15 +163,15 @@ class Controller:
                     binary_row.append(1)
                     self.point_spaces.append((y, x))
                     self.reachable_spaces.append((y, x))
-            self.numpy_maze.append(binary_row)
+            self.maze.append(binary_row)
 
 
 class MovableObject(Object):
     def __init__(self, surface, x, y, size: int, color=(255, 0, 0), circle: bool = False):
         super().__init__(surface, x, y, size, color, circle)
-        self.cur_dir = Direction.NONE
-        self.dir_buffer = Direction.NONE
-        self.last_dir = Direction.NONE
+        self.cur_dir = NONE
+        self.dir_buffer = NONE
+        self.last_dir = NONE
         self.queue = []
         self.next_target = None
 
@@ -215,24 +188,24 @@ class MovableObject(Object):
     def collides_with_wall(self, position):
         collision_rect = pygame.Rect(position[0], position[1], self.size, self.size)
         collides = False
-        walls = self.renderer.get_walls()
+        walls = self.render.get_walls()
         for wall in walls:
             collides = collision_rect.colliderect(wall.get_shape())
             if collides:
                 break
         return collides
 
-    def check_collision_in_direction(self, direction: Direction):
+    def check_reachable(self, direction: int):
         des_position = (0, 0)
-        if direction == Direction.NONE:
+        if direction == NONE:
             return False, des_position
-        if direction == Direction.UP:
+        if direction == UP:
             des_position = (self.x, self.y - 1)
-        elif direction == Direction.DOWN:
+        elif direction == DOWN:
             des_position = (self.x, self.y + 1)
-        elif direction == Direction.LEFT:
+        elif direction == LEFT:
             des_position = (self.x - 1, self.y)
-        elif direction == Direction.RIGHT:
+        elif direction == RIGHT:
             des_position = (self.x + 1, self.y)
 
         return self.collides_with_wall(des_position), des_position
@@ -245,38 +218,38 @@ class Ghost(MovableObject):
 
 
 class Hero(MovableObject):
-    def __init__(self, in_surface, x, y, in_size: int):
-        super().__init__(in_surface, x, y, in_size, (255, 255, 0), False)
-        self.last_non_colliding_position = (0, 0)
+    def __init__(self, in_surface, x, y, size: int):
+        super().__init__(in_surface, x, y, size, (255, 255, 0), False)
+        self.last_position = (0, 0)
 
     def tick(self):
         if self.x < 0:
-            self.x = self.renderer.width
+            self.x = self.render.width
 
-        if self.x > self.renderer.width:
+        if self.x > self.render.width:
             self.x = 0
 
-        self.last_non_colliding_position = self.get_position()
+        self.last_position = self.get_position()
 
-        if self.check_collision_in_direction(self.dir_buffer)[0]:
+        if self.check_reachable(self.dir_buffer)[0]:
             self.automatic_move(self.cur_dir)
         else:
             self.automatic_move(self.dir_buffer)
             self.current_direction = self.dir_buffer
 
         if self.collides_with_wall((self.x, self.y)):
-            self.set_position(self.last_non_colliding_position[0], self.last_non_colliding_position[1])
+            self.set_position(self.last_position[0], self.last_position[1])
 
-    def automatic_move(self, in_direction: Direction):
-        collision_result = self.check_collision_in_direction(in_direction)
+    def automatic_move(self, in_direction: int):
+        if_collides = self.check_reachable(in_direction)
 
-        desired_position_collides = collision_result[0]
-        if not desired_position_collides:
-            self.last_working_direction = self.cur_dir
-            desired_position = collision_result[1]
+        next_pos = if_collides[0]
+        if not next_pos:
+            self.last_dir = self.cur_dir
+            desired_position = if_collides[1]
             self.set_position(desired_position[0], desired_position[1])
         else:
-            self.cur_dir = self.last_working_direction
+            self.cur_dir = self.last_dir
 
     def draw(self):
         half_size = self.size / 2
@@ -287,19 +260,19 @@ if __name__ == "__main__":
     unified_size = 32
     pacman_game = Controller()
     size = pacman_game.size
-    game_renderer = Renderer(size[0] * unified_size, size[1] * unified_size)
+    game_render = Render(size[0] * unified_size, size[1] * unified_size)
 
-    for y, row in enumerate(pacman_game.numpy_maze):
+    for y, row in enumerate(pacman_game.maze):
         for x, column in enumerate(row):
             if column == 0:
-                game_renderer.add_wall(Wall(game_renderer, x, y, unified_size))
+                game_render.add_wall(Wall(game_render, x, y, unified_size))
 
     for i, ghost_spawn in enumerate(pacman_game.ghost_spawns):
         translated = translate_maze_to_screen(ghost_spawn)
-        ghost = Ghost(game_renderer, translated[0], translated[1], unified_size, pacman_game,
+        ghost = Ghost(game_render, translated[0], translated[1], unified_size, pacman_game,
                       pacman_game.ghost_colors[i % 4])
-        game_renderer.add_game_object(ghost)
+        game_render.add_game_object(ghost)
 
-    pacman = Hero(game_renderer, unified_size, unified_size, unified_size)
-    game_renderer.add_hero(pacman)
-    game_renderer.tick(120)
+    pacman = Hero(game_render, unified_size, unified_size, unified_size)
+    game_render.add_hero(pacman)
+    game_render.tick(120)
